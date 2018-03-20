44e8a5663cb1aea11276312dea2a6ca59ed1235c
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-03-20 08:00:44 -0700

    unit test for completed added

--- src/tests/Completed.test.js
+++ src/tests/Completed.test.js
@@ -1,9 +1,22 @@
 import React from 'react'; 
-import { shallow } from 'enzyme'; 
-import Completed from '../components/Completed/Completed'; 
+import { shallow, mount } from 'enzyme'; 
+import Completed from '../components/Completed/Completed';
+import CompletedItem from '../components/CompletedItem/CompletedItem';  
 
 describe('Completed', () => {
     it('renders without crashing', () => {
         shallow(<Completed items={[]} />); 
+    });
+    it('displays title', () => {
+        const wrapper = shallow(<Completed items={[]} />); 
+        expect(wrapper.contains('Completed')).toEqual(true); 
+    });
+    it('contains CompletedItem when passed items as props', () => {
+        const wrapper = mount(<Completed items={['test']} />);
+        expect(wrapper.find(CompletedItem).exists()).toEqual(true);  
+    });
+    it('does not contain CompletedItem when passed no items as props', () => {
+        const wrapper = mount(<Completed items={[]} />);
+        expect(wrapper.find(CompletedItem).exists()).toEqual(false);  
     });
 }); 
