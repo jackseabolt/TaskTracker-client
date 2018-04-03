95f55ef88e8a1e93264a0173448b49336267d159
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-04-03 11:19:59 -0700

    config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+
+if (process.env.NODE_ENV === 'development'){
+    module.exports = {
+        REACT_APP_API_URL: 'http://localhost:8080'
+    }  
+} else {
+    module.exports = {
+        REACT_APP_API_URL: 'https://trasktracker-backend.herokuapp.com/'
+    }
+}
